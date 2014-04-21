9de6d5e90c3d379d5f1bd9707bbfae6a641d9542
Adel Benlagra <benlagra@itp.phys.ethz.ch>
2014-04-21 16:11:51 +0200

    Last changes to cachevector.R

--- ProgAss/02/cachevector.R
+++ ProgAss/02/cachevector.R
@@ -1,7 +1,7 @@
 makeVector <- function(x = numeric()) {
-       m <- NULL
+       m <- 3
        set <- function(y) {
-              x <<- y
+             x <<- y
               m <<- NULL
        }
        get <- function() x
@@ -10,4 +10,16 @@ makeVector <- function(x = numeric()) {
        list(set = set, get = get,
             setmean = setmean,
             getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+       m <- x$getmean()
+       if(!is.null(m)) {
+              message("getting cached data")
+              return(m)
+       }
+       data <- x$get()
+       m <- mean(data, ...)
+       x$setmean(m)
+       m
 }
